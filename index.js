cff1a647336526b81f8c4592ce7d314601eaa3b5
Yury Michurin <yury.michurin@gmail.com>
2017-12-31 23:35:07 +0200

    No cluster trigger, takes much longer though

--- index.js
+++ index.js
@@ -1,48 +1,16 @@
 const logger = require("./winston");
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
 
 const test = () => {
 
     const done = () => {
         setTimeout(() => {
             test();
-        }, 100);
+        }, 1);
     };
     
-    process.send({ cmd: 'logThis', msg: "testing " + new Date() });
-    done();
+    logger.debug("testing log")
+    done()
 };
 
-process.on('uncaughtException', (err) => {
-  console.log('whoops! there was an error', err.stack);
-  process.exit(2);
-});
 
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
-
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  function messageHandler(msg) {
-    if ( msg.cmd && msg.cmd === 'logThis' && msg.msg ) {
-      logger.debug( "Worker " + this.workerId, msg.msg );
-    }
-  }
-
-  for (const id in cluster.workers) {
-    cluster.workers[id].on('message', messageHandler.bind( { workerId: id } ) );
-  }
-
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-  });
-} else {
-  console.log(`Worker ${process.pid} started`);
-  test();
-}
-
-
+test();
