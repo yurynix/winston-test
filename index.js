6e67126fa2954496a3d3ddd465e9a236a2bcf2f6
Yury Michurin <yury.michurin@gmail.com>
2017-12-31 23:09:52 +0200

    Add date

--- index.js
+++ index.js
@@ -10,10 +10,14 @@ const test = () => {
         }, 100);
     };
     
-    logger.debug("testing log")
-    done()
+    process.send({ cmd: 'logThis', msg: "testing " + new Date() });
+    done();
 };
 
+process.on('uncaughtException', (err) => {
+  console.log('whoops! there was an error', err.stack);
+  process.exit(2);
+});
 
 if (cluster.isMaster) {
   console.log(`Master ${process.pid} is running`);
@@ -23,14 +27,22 @@ if (cluster.isMaster) {
     cluster.fork();
   }
 
+  function messageHandler(msg) {
+    if ( msg.cmd && msg.cmd === 'logThis' && msg.msg ) {
+      logger.debug( "Worker " + this.workerId, msg.msg );
+    }
+  }
+
+  for (const id in cluster.workers) {
+    cluster.workers[id].on('message', messageHandler.bind( { workerId: id } ) );
+  }
+
   cluster.on('exit', (worker, code, signal) => {
     console.log(`worker ${worker.process.pid} died`);
   });
 } else {
-  // Workers can share any TCP connection
-  // In this case it is an HTTP server
-  test();
   console.log(`Worker ${process.pid} started`);
+  test();
 }
 
 
