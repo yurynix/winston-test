d82b8c184407c0dc90b4b02178fc50db52f9dae6
Yury Michurin <yury.michurin@gmail.com>
2017-12-31 16:38:48 +0200

    Reproduction of error with cluster

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const logger = require("./winston");
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
 const test = () => {
 
@@ -12,4 +14,23 @@ const test = () => {
     done()
 };
 
-test();
+
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+  test();
+  console.log(`Worker ${process.pid} started`);
+}
+
+
